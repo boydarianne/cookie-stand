26d9a95e8d4854bc2dee477784cee707e74f9160
Arianne Boyd <boydarianne@yahoo.com>
2023-08-30 13:42:33 -0400

    first ACP to github

---
+++ app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const seattleStore = {
+    minCust: 
+    maxCust: 
+    cookiesPerHr:
+
+    getAnyNumber: function(min, max){
+        let minCust= [];
+        let maxCust= [];
+        return 
+    }
+    
+}
+
+const tokyoStore = {
+    minCust:
+    maxCust:
+    avgCookie:
+}
+
+const dubaiStore = {
+    minCust:
+    maxCust:
+    avgCookie:
+}
+
+const parisStore = {
+    minCust:
+    maxCust:
+    avgCookie:
+}
+
+const limaStore = {
+    minCust:
+    maxCust:
+    avgCookie:
+}
